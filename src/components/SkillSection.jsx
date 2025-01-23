40e8c3a1f1c0f1c54f96646793d857146914a944
turtlebeasts <179turtleshells@gmail.com>
2025-01-24 01:55:40 +0530

    responsive skills section

--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -24,13 +24,13 @@ export default function SkillSection() {
   ];
   return (
     <div className="py-10 px-4 md:px-40 bg-white text-center">
-      <h1 className="text-6xl font-bold">Skills</h1>
+      <h1 className="text-4xl md:text-6xl font-bold mb-10">Skills</h1>
       <div className="flex justify-between">
         {images.map((item, key) => (
           <img
             src={item.image}
             alt={item.name}
-            className="w-24 h-24"
+            className="w-14 h-14 md:w-24 md:h-24"
             key={key}
           />
         ))}
